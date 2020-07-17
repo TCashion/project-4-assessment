ff41e57b683465e72c60a576d8f4980d16aa9be7
TCashion <58757351+TCashion@users.noreply.github.com>
2020-07-17 13:27:02 -0600

    Updates

---
+++ precache-manifest.ae38a41a8be71da88253f18e9f824827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccff93c3644f31c88957e45deddecc84",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "c0685942f54e5890c0bc",
+    "url": "/project-4-assessment/static/css/main.567f5566.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0685942f54e5890c0bc",
+    "url": "/project-4-assessment/static/js/main.06076427.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
